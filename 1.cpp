fc642aba18be5811c4d0a5d486ca4e2db031aa94
陈宇希 <cyx_chenyuxi@163.com>
2025-09-06 19:33:56 +0800

    new start

---
+++ 1.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int q[100000];
+
+void quick_sort(int q[], int l, int r) {
+  if (l >= r) return;
+  int mid = (l + r) / 2;
+  int x = q[mid];
+  int i = l - 1;
+  int j = r + 1;
+  while (i < j) {
+    while (q[++i] < x);
+    while (q[--j] > x);
+    if (i < j) {
+      swap(q[i], q[j]);
+    }
+  }
+  quick_sort(q, l, j);
+  quick_sort(q, j + 1, r);
+}
+
+int main() {
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> q[i];
+  }
+  quick_sort(q, 0, n - 1);
+  for (int i = 0; i < n; i++) {
+    cout << q[i] << " ";
+  }
+  cout << endl;
+}
